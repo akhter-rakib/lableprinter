202b908a039c258fa06dfadfbfe52b23f961472c
akter_rakib <rakibccj@gmail.com>
2020-10-03 12:58:55 +0600

    project initiate

---
+++ src/main/java/com/ctech/lableprinter/LableprinterApplication.java
@@ -0,0 +1,13 @@
+package com.ctech.lableprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LableprinterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LableprinterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ctech/lableprinter/LableprinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctech.lableprinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LableprinterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
